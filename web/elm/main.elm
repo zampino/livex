8c93b8a581c4398889f72533ff40d176e50410ab
Andrea Amantini <lo.zampino@gmail.com>
2016-03-22 14:42:00 +0100

    the trace is left, but extremely unperformant

--- web/elm/main.elm
+++ web/elm/main.elm
@@ -72,51 +72,64 @@ action (idx, prop, value) state =
 stateSignal =
   Signal.foldp action initState stateChangeEvents
 
+type alias DState = { drawing : List Form, last_position : Pos, paths : List Form }
 
-main =
-  Signal.map2 view (every millisecond) stateSignal
+sgm : Pos -> Pos -> List Form
+sgm start stop =
+  case start of
+    (0, 0) -> []
+    other -> [traced (solid black) (segment other stop)]
 
+compositeSignal : Signal (Time, State)
+compositeSignal =
+  Signal.map2 (\x y -> (x, y)) (every millisecond) stateSignal
 
--- type DrawState
--- draw_action : Time -> DrawState -> DrawState
--- draw_action t ds =
---
---   -- ...
---   let
---     (pos, _, elem) =
---       render time state
---     trace =
---       traced dotted black segment ds.last_position pos
---     updated = { ds | last_position = pos }
---   in
---     { updated | drawing = }
-
-{--
-view time ds =
-  collage 600 600 (render time ds)
-
---}
-
-
-view : Time -> State -> Element
-view time state =
-  collage 900 900 (render time state)
-
-third (a, b, c) = c
-
-render : Time -> State -> List Form
-render time state =
+draw_action : (Time, State) -> DState -> DState
+draw_action (time, state) ds =
   let
-    comp =
-      (time, (0,0), [])
-      |> rotor (safeGet "c1" state)
-      |> rotor (safeGet "c2" state)
-      |> rotor (safeGet "c3" state)
+    last_position = ds.last_position
+    (new_pos, form_list) = render time state
+    dsgm = sgm last_position new_pos
+    updated = { ds | last_position = new_pos, paths = ds.paths ++ dsgm }
   in
-    third comp
+    { updated | drawing = form_list ++ updated.paths }
 
-rotor : Geometry -> (Time, Pos, List Form) -> (Time, Pos, List Form)
-rotor geometry (time, center, acc) =
+lift : State -> DState
+lift state =
+  { last_position = (0, 0)
+  , drawing = []
+  , paths = []
+  }
+
+drawingSignal : Signal DState
+drawingSignal =
+  Signal.foldp draw_action (lift initState) compositeSignal
+
+main : Signal Element
+main =
+  Signal.map view drawingSignal
+
+view : DState -> Element
+view ds =
+  collage 600 600 ds.drawing
+
+-- main =
+--   Signal.map2 view (every millisecond) stateSignal
+
+
+-- view : Time -> State -> Element
+-- view time state =
+--   collage 900 900 (render time state)
+
+render : Time -> State -> (Pos, List Form)
+render time state =
+  ((0,0), [])
+  |> rotor time (safeGet "c1" state)
+  |> rotor time (safeGet "c2" state)
+  |> rotor time (safeGet "c3" state)
+
+rotor : Time -> Geometry -> (Pos, List Form) -> (Pos, List Form)
+rotor time geometry (center, acc) =
   let
     angle =
       2 * pi * geometry.omega * ( time / 1000 )
@@ -137,7 +150,7 @@ rotor geometry (time, center, acc) =
     new_list =
       acc ++ elm
   in
-    (time, pos, new_list)
+    (pos, new_list)
 
 
 
